d385238d691d4aa1a1a7b2f2a2d3b490f21b364b
Marco Sternini - marco.sternini2@studio.unibo.it <STUDENTI\marco.sternini2@isi-raptor03.campusfc.dir.unibo.it>
2023-03-27 19:06:28 +0200

    Constant memory optimization

--- src/cuda-sph.cu
+++ src/cuda-sph.cu
@@ -28,6 +28,12 @@
  * SOFTWARE.
  *
  ****************************************************************************/
+
+/* HPC - Assignment 2022/2023
+   Marco Sternini - marco.sternini2@studio.unibo.it
+   0000971418
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -44,12 +50,16 @@
 __constant__ float Gx = 0.0, Gy = -10.0;   // external (gravitational) forces
 __constant__ float REST_DENS = 300;    // rest density
 __constant__ float GAS_CONST = 2000;   // const for equation of state
-const __constant__ float H = 16;             // kernel radius
-const __constant__ float EPS = 16;           // equal to H
 __constant__ float MASS = 2.5;         // assume all particles have the same mass
 __constant__ float VISC = 200;         // viscosity constant
 __constant__ float DT = 0.0007;        // integration timestep
 __constant__ float BOUND_DAMPING = -0.5;
+__constant__ float POLY6;
+__constant__ float SPIKY_GRAD;
+__constant__ float VISC_LAP;
+__constant__ float HSQ; 
+const __constant__ float H = 16;             // kernel radius
+const __constant__ float EPS = 16;           // equal to H
 
 #define WINDOW_WIDTH 3000
 #define WINDOW_HEIGHT 2000
@@ -80,26 +90,23 @@ int n_particles = 0;    // number of currently active particles
 
 __global__ void k_compute_density_pressure( particles_t *ps, int n )
 {
-    const float HSQ = H * H;    // radius^2 for optimization
-    const int id = threadIdx.x + blockIdx.x * blockDim.x;     
-
     /* Smoothing kernels defined in Muller and their gradients adapted
        to 2D per "SPH Based Shallow Water Simulation" by Solenthaler
        et al. */
-    const float POLY6 = 4.0 / (M_PI * pow(H, 8));
+    const int i = threadIdx.x + blockIdx.x * blockDim.x;     
 
-    if (id < n) {
-        ps->rho[id] = 0.0;
+    if (i < n) {
+        ps->rho[i] = 0.0;
         for (int j=0; j < n; j++) {
-            const float dx = ps->x[j] - ps->x[id];
-            const float dy = ps->y[j] - ps->y[id];
+            const float dx = ps->x[j] - ps->x[i];
+            const float dy = ps->y[j] - ps->y[i];
             const float d2 = dx*dx + dy*dy;
 
             if (d2 < HSQ) {
-                ps->rho[id] += MASS * POLY6 * pow(HSQ - d2, 3.0);
+                ps->rho[i] += MASS * POLY6 * pow(HSQ - d2, 3.0);
             }
         }
-        ps->p[id] = GAS_CONST * (ps->rho[id] - REST_DENS);
+        ps->p[i] = GAS_CONST * (ps->rho[i] - REST_DENS);
     }   
 }
 
@@ -108,9 +115,6 @@ __global__ void k_compute_forces( particles_t *ps, int n )
     /* Smoothing kernels defined in Muller and their gradients adapted
        to 2D per "SPH Based Shallow Water Simulation" by Solenthaler
        et al. */
-    const float SPIKY_GRAD = -10.0 / (M_PI * pow(H, 5));
-    const float VISC_LAP = 40.0 / (M_PI * pow(H, 5));
-    const float EPS = 1e-6;
     const int i = threadIdx.x + blockIdx.x * blockDim.x;
 
     if (i < n) {
@@ -173,12 +177,21 @@ __global__ void k_integrate( particles_t *ps, int n )
     }
 }
 
+/**
+  * Calculates the avg velocity of the particles. The kernel operates a 
+  * reduction. Each thread cooperates with threads of its block to obtain
+  * a partial result that will be returned to the host. 
+  * Unused threads fill the shared memory with zeroes to not interfere with
+  * the result. 
+  */
 __global__ void k_avg_velocities( particles_t *ps, int n, float* o_result )
 {
     __shared__ float s_results[BLKDIM];
     const int gind = threadIdx.x + blockIdx.x * blockDim.x;
     const int lind = threadIdx.x;
-    
+   
+    /* If the thread is mapped to a non-existent
+       particle put 0 */
     if (gind > n) {
         s_results[lind] = 0;
     } else {
@@ -305,8 +318,26 @@ void init_sph( int n )
    free(particles);
 }
 
+/**
+  * Initialize device __constant__ memory
+  */ 
+void init_constants( void ) 
+{
+    size_t size = sizeof(float);
+    float H_HSQ = H * H; 
+    float H_POLY6 = 4.0 / (M_PI * pow(H, 8));
+    float H_SPIKY_GRAD = -10.0 / (M_PI * pow(H, 5));
+    float H_VISC_LAP = 40.0 / (M_PI * pow(H, 5));
+
+    cudaMemcpyToSymbol(HSQ, &H_HSQ, size);
+    cudaMemcpyToSymbol(POLY6, &H_POLY6, size);
+    cudaMemcpyToSymbol(SPIKY_GRAD, &H_SPIKY_GRAD, size);
+    cudaMemcpyToSymbol(VISC_LAP, &H_VISC_LAP, size);
+}
+
 void update()
 {
+    /* Launch all the kernels in queue */
     k_compute_density_pressure<<<grid, block>>>(d_particles, n_particles);
     k_compute_forces<<<grid, block>>>(d_particles, n_particles);
     k_integrate<<<grid, block>>>(d_particles, n_particles);
@@ -340,9 +371,10 @@ int main(int argc, char **argv)
 
     
     init_sph(n);
+    init_constants();
     block = dim3(BLKDIM);
     grid = dim3((n_particles+BLKDIM-1)/BLKDIM);
-    float *partial_avg_out = (float*)malloc(sizeof(float) * grid.x), avg_velocity = 0;
+    float *partial_avg_out = (float*)malloc(sizeof(float) * grid.x), avg_velocity = 0; // device reduction output
     double tstart, tstop;
     tstart = hpc_gettime();
     for (int s=0; s<nsteps; s++) {
@@ -355,7 +387,9 @@ int main(int argc, char **argv)
 	avg_velocity = 0;
 	cudaMemcpy(partial_avg_out, d_avg_out, sizeof(float) * grid.x, cudaMemcpyDeviceToHost);
         for (int i=0; i < grid.x; i++) {
-	   avg_velocity += partial_avg_out[i];
+	    /* The host uses the partial results of each block and 
+	       calculates the avg_velocity */
+	    avg_velocity += partial_avg_out[i];
 	}
 
 	if (s % 10 == 0)
@@ -365,17 +399,16 @@ int main(int argc, char **argv)
     tstop = hpc_gettime();
     printf("Elapsed time: %fs ", tstop - tstart);
     
-   cudaFree(d_x);
-   cudaFree(d_y);
-   cudaFree(d_vx);
-   cudaFree(d_vy);
-   cudaFree(d_fx);
-   cudaFree(d_fy);
-   cudaFree(d_p);
-   cudaFree(d_rho);
-   cudaFree(d_particles);
-   cudaFree(d_avg_out);
-   free(partial_avg_out);
-   
-   return EXIT_SUCCESS;
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_vx);
+    cudaFree(d_vy);
+    cudaFree(d_fx);
+    cudaFree(d_fy);
+    cudaFree(d_p);
+    cudaFree(d_rho);
+    cudaFree(d_particles);
+    cudaFree(d_avg_out);
+    free(partial_avg_out);
+    return EXIT_SUCCESS;
 }
